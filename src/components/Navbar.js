cdbca7502793c32abb97d7691e27bc8e12770547
Madhulika <madhulika.ku@gmail.com>
2023-02-28 13:00:45 +0530

    navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div className="navbar">
+      <img src="/images/logo.png" alt="logo" className="logo" />
+      <div className="title">Meme Generator</div>
+    </div>
+  );
+}
